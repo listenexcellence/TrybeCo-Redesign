9c8907431a8dd2ee624646fa2f6b49cbb0525898
Bright099 <brihtphakson@gmail.com>
2023-02-21 16:10:34 +0100

    aain

--- Assets/script.js
+++ Assets/script.js
@@ -40,8 +40,7 @@ function displayTime() {
   ];
   month = monthNames[month];
 
-  document.getElementById("year").innerHTML = 
-  "ⓒ "+ year;
+  document.getElementById("year").innerHTML = "ⓒ " + year;
 
   document.getElementById("clock").innerHTML =
     day +
@@ -154,9 +153,72 @@ function sendMail(params) {
     });
 }
 
-
 var cursor = document.getElementById("cursor");
-document.body.addEventListener("mousemove", function(e) {
-  cursor.style.left = e.clientX + "px",
-    cursor.style.top = e.clientY + "px";
+document.body.addEventListener("mousemove", function (e) {
+  (cursor.style.left = e.clientX + "px"), (cursor.style.top = e.clientY + "px");
 });
+
+const Quote_Array = [
+  "Coding like poetry should be short and concise. ― Santosh Kalwar",
+  "It's not a bug; its an undocumented feature. ― Anonymous",
+  "First, solve the problem. Then, write the code. - John Johnson",
+  "Code is like humor. When you have to explain it, it's bad. - Cory House",
+  "Make it work, make it right, make it fast. - Kent Beck",
+  "Clean code always looks like it was written by someone who cares. — Robert C. Martin",
+  "Of course, bad code can be cleaned up. But it's very expensive. — Robert C. Martin",
+  "Programming is the art of algorithm design and the craft of debugging errant code. - Ellen Ullman",
+  "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs and the Universe trying to produce bigger and better idiots. So far, the Universe is winning. ― Rick Cook",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand. ― Martin Fowler",
+  "Experience is the name everyone gives to their mistakes. - Oscar Wilde",
+  "Programming is the art of telling another human being what one wants the computer to do. ― Donald Ervin Knuth",
+  "Confusion is part of programming. ― Felienne Hermans",
+  "No matter which field of work you want to go in, it is of great importance to learn at least one programming language. ― Ram Ray",
+  "If we want users to like our software, we should design it to behave like a likable person.  - Alan Cooper",
+  "Quality is a product of a conflict between programmers and testers. ― Yegor Bugayenk",
+  "Everybody should learn to program a computer because it teaches you how to think. - Steve Jobs",
+  "I taught myself how to program computers when I was a kid, bought my first computer when I was 10, and sold my first commercial program when I was 12. - Elon Musk",
+  "Software and cathedrals are much the same — first we build them, then we pray.",
+  "Web development is difficult, only then it is fun to do. You just have to set your standards. If it were to be easy, would anyone do it? ― Olawale Daniel",
+  "Programmers seem to be changing the world. It would be a relief, for them and for all of us, if they knew something about it.  - Ellen Ullman",
+  "Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program. - Linus Torvalds",
+  "Programmer: A machine that turns coffee into code. - Anonymous",
+  "When I wrote this code, only God and I understood what I did. Now only God knows.  - Anonymous",
+  "I'm not a great programmer; I'm just a good programmer with great habits. ― Kent Beck",
+  "You might not think that programmers are artists, but programming is an extremely creative profession. It's logic-based creativity. - John Romero",
+  "Programming is learned by writing programs. ― Brian Kernighan",
+  "Software comes from heaven when you have good hardware. - Ken Olsen",
+  "There is always one more bug to fix.  - Ellen Ullman",
+  "If debugging is the process of removing bugs, then programming must be the process of putting them in. – Sam Redwine",
+  "Talk is cheap. Show me the code. ― Linus Torvalds",
+  "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code. – Dan Salomon",
+  "If, at first, you do not succeed, call it version 1.0. ― Khayri R.R. Woulfe",
+  "Computers are fast; developers keep them slow. – Anonymous",
+];
+
+const onLoad = () => {
+  let number = Math.floor(Math.random() * 10 + 1);
+  if ((number = Quote_Array[Math.floor(Math.random() * 35 + 1)])) {
+    document.getElementById("load-quote").innerHTML =
+      Quote_Array[Math.floor(Math.random() * 10 + 1)];
+  }
+};
+window.onload = onLoad();
+
+let count = 1;
+function incrementCount() {
+  // If count is already at 100, reset it to 1
+  if (count === 100) {
+    document.getElementById("preload").style.display = "none";
+  } else {
+    count++;
+  }
+  // Display the current count in the console
+  console.log(count);
+}
+
+// Call the incrementCount function repeatedly every second
+setInterval(incrementCount, 60);
+
+const interval = setInterval(function () {
+  document.getElementById("counter").innerHTML = count;
+}, 9);
